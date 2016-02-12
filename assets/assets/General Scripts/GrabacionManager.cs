9f7d66b49343c07f52806426662ec4800ee84ecc
Gustavo E Bonilla <gbonilla@unbosque.edu.co>
2016-02-12 00:28:53 -0500

    Implementacion de modal de confirmacion para ingresar el nombre de la rutina a grabarse.

--- assets/assets/General Scripts/GrabacionManager.cs
+++ assets/assets/General Scripts/GrabacionManager.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
 using System.Collections;
+using SimpleJSON;
 
-/**
- * Clase que permite gestionar la grabacion de los movimientos que se realizan en el Kinect
- * 
- * @author gbonilla@unbosque.edu.co - efrancor@unbosque.edu.co
- */
-public class GrabacionManager : MonoBehaviour {
-	
+using System.IO;
+
+/// <summary>
+/// Grabacion manager.
+/// Clase que permite gestionar la grabacion de los movimientos que se realizan en el Kinect
+/// 
+/// </summary>
+public class GrabacionManager : MonoBehaviour, GrabadorMovimiento.GrabacionStatus {
+
 	private Rect windowRect;
 	
 	private int bW = 200;
@@ -16,23 +19,35 @@ public class GrabacionManager : MonoBehaviour {
 	private int GH = 170;
 
 	private GrabadorMovimiento grabadorMovimiento;
+	private string nombreRutina = "";
+	private string path = "Recordings/playback/";
 	
 	public GUIText CalibrationText;
 	public KinectManager kinectManager;
 	
 	// Use this for initialization
 	void Start () {
-		grabadorMovimiento = new GrabadorMovimiento (kinectManager);
+		grabadorMovimiento = new GrabadorMovimiento (kinectManager, this);
 	}
 	
 	void OnGUI () {
 		GUI.BeginGroup(new Rect (((Screen.width/2)- (gW/2)),((Screen.height/2)-(gW/2)), gW, GH));
-		if (GUI.Button(new Rect(0,0,bW,bH),"Iniciar Grabacion")) {
-			grabadorMovimiento.StartRecord();
-		}		
-		
-		if(GUI.Button(new Rect(0,120,bW,bH),"Detener Grabacion")){		
-			grabadorMovimiento.StopRecord();
+		if (!grabadorMovimiento.isRecording ()) {
+			if (GUI.Button (new Rect (0, 0, bW, bH), "Iniciar Grabacion")) {
+					grabadorMovimiento.StartRecord ();
+			}		
+		} else {
+			if (grabadorMovimiento.isWaitingForRoutineName()) {
+				GUI.Label(new Rect (0, 0, bW, bH), "Ingrese el nombre de la rutina");
+				nombreRutina = GUI.TextField(new Rect (0, 60, bW, bH), nombreRutina);
+				if (GUI.Button(new Rect (0, 120, bW, bH), "Ingresar")) {
+					grabadorMovimiento.setRoutineName(nombreRutina);
+				}
+			} else {
+			if (GUI.Button (new Rect (0, 120, bW, bH), "Detener Grabacion")) {		
+					grabadorMovimiento.StopRecord ();
+				}
+			}
 		}
 		GUI.EndGroup();
 	}
@@ -40,11 +55,26 @@ public class GrabacionManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (grabadorMovimiento.isRecording()) {
+		if (grabadorMovimiento.isRecording() && !grabadorMovimiento.isWaitingForRoutineName()) {
 			if (grabadorMovimiento.record().Length > 0) {
 				Debug.Log(grabadorMovimiento.record());
 			}
 		}
+	}
+
+	/// <summary>
+	/// Metodo que indica cuando se ha completado una grabacion.
+	/// </summary>
+	/// <param name="pathFile">Ruta del Archivo</param>
+	public void grabacionCompletada(string pathFile) {
+		string rutinasJsonStr = File.ReadAllText (@path + "rutinas.json");
+		Debug.Log(rutinasJsonStr);
+		ArrayList rutinasData = (ArrayList)JSON.JsonDecode(rutinasJsonStr);
+		rutinasData.Add(pathFile);
+		rutinasJsonStr = JSON.JsonEncode(rutinasData);
+		var sr = File.CreateText(@path + "rutinas.json");
+		sr.WriteLine (rutinasJsonStr);
+		sr.Close();
 	}
 }
 
--- assets/assets/General Scripts/GrabadorMovimiento.cs
+++ assets/assets/General Scripts/GrabadorMovimiento.cs
@@ -4,37 +4,81 @@ using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using Kinect;
 
-/**
- * Clase que permite grabar los movimientos que se realizan en el Kinect
- * 
- * @author gbonilla@unbosque.edu.co - efrancor@unbosque.edu.co
- * @link {http://gamedev.stackexchange.com/questions/21544/how-do-you-save-game-state/27546#27546}
- */
+/// <summary>
+/// Grabador movimiento. Clase que realiza la grabacion de movimientos.
+/// gbonilla@unbosque.edu.co - efrancor@unbosque.edu.co
+/// </summary>
 public class GrabadorMovimiento {
+
+	/// <summary>
+	/// Interface que establece como esta el status de la grabacion de un archivo.
+	/// </summary>
+	public interface GrabacionStatus {
+		/// <summary>
+		/// Metodo que indica cuando se ha completado una grabacion.
+		/// </summary>
+		/// <param name="pathFile">Ruta del Archivo</param>
+		void grabacionCompletada(string pathFile);
+	}
 
 	private string path = "Recordings/playback/";
 	private static string ext = ".bcm";
 	private bool recording = false;
+	private bool waitingForRoutineName = false;
 	private List<KinectWrapper.NuiSkeletonFrame> currentData = new List<KinectWrapper.NuiSkeletonFrame>();
 	private string nameGrabador = "movement";
 	private int fileCount;
 	private KinectManager kinectManager;
+	private GrabacionStatus grabacionStatusObj;
 
-	public GrabadorMovimiento(KinectManager kinectManager) {
+	/// <summary>
+	/// Inicializa una nueva instancia de la clase <see cref="GrabadorMovimiento"/>.
+	/// </summary>
+	/// <param name="kinectManager">Kinect manager.</param>
+	/// <param name="grabacionStatus">Grabacion status.</param>
+	public GrabadorMovimiento(KinectManager kinectManager, GrabacionStatus grabacionStatus) {
 		this.kinectManager = kinectManager;
+		this.grabacionStatusObj = grabacionStatus;
 	}
 
+	/// <summary>
+	/// Inicia la grabacion.
+	/// </summary>
 	public void StartRecord() {
+		waitingForRoutineName = true;
 		recording = true;
 		Debug.Log("start recording");
 		currentData = new List<KinectWrapper.NuiSkeletonFrame>();;
 	}
 
+	/// <summary>
+	/// Dice si se esta grabando en el momento.
+	/// </summary>
+	/// <returns><c>true</c>, si se esta grabando, <c>false</c> de lo contrario.</returns>
 	public bool isRecording() {
 		return recording;
 	}
 
-	
+	/// <summary>
+	/// Indica si se esta esperando a que se ingrese el nombre de la rutina.
+	/// </summary>
+	/// <returns><c>true</c>, si se espera por el nombre de la rutina, <c>false</c> de lo contrario.</returns>
+	public bool isWaitingForRoutineName() {
+		return waitingForRoutineName;
+	}
+
+	/// <summary>
+	/// Establece el nombre de la rutina a grabarse.
+	/// </summary>
+	/// <param name="name">Name.</param>
+	public void setRoutineName(string name) {
+		this.waitingForRoutineName = false;
+		this.nameGrabador = name;
+	}
+
+	/// <summary>
+	/// Graba cada frame de movimiento que se captura con el Kinect
+	/// </summary>
 	public string record() {
 		string recordMessage = "";
 		if (KinectWrapper.PollSkeleton (ref kinectManager.smoothParameters, ref kinectManager.skeletonFrame)) {                
@@ -44,15 +88,17 @@ public class GrabadorMovimiento {
 		return recordMessage;
 	}
 
-	
+	/// <summary>
+	/// Para la grabacion de la rutina, 
+	/// guarda los datos en el archivo 
+	/// y notifica la ruta y el nombre del archivo.
+	/// </summary>
 	public void StopRecord() {
 		recording = false;
 
-
-		//edit by lxjk
 		string filePath = path + nameGrabador + ext;
 		FileStream output = new FileStream(@filePath,FileMode.Create);
-		//end lxjk
+
 		BinaryFormatter bf = new BinaryFormatter();
 		
 		SerialSkeletonFrame[] data = new SerialSkeletonFrame[currentData.Count];
@@ -61,11 +107,16 @@ public class GrabadorMovimiento {
 		}
 		bf.Serialize(output, data);
 		output.Close();
-		fileCount++;
+		grabacionStatusObj.grabacionCompletada (filePath);
 
 		Debug.Log("stop recording");
 	}
 
+	/// <summary>
+	/// Convierte el SkeletonFrame de <see cref="KinectWrapper.NuiSkeletonFrame"/> a <see cref="Kinect.NuiSkeletonFrame"/>
+	/// </summary>
+	/// <returns>The skeleton frame.</returns>
+	/// <param name="frame">Frame.</param>
 	private Kinect.NuiSkeletonFrame convertSkeletonFrame(KinectWrapper.NuiSkeletonFrame frame) {
 		Kinect.NuiSkeletonFrame frameConverted;
 		frameConverted.dwFlags = frame.dwFlags;
@@ -82,6 +133,11 @@ public class GrabadorMovimiento {
 		return frameConverted;
 	}
 
+	/// <summary>
+	/// Convierte el SkeletonData de <see cref="KinectWrapper.NuiSkeletonData"/> a <see cref="Kinect.NuiSkeletonData"/>
+	/// </summary>
+	/// <returns>The skeleton data.</returns>
+	/// <param name="data">Data.</param>
 	private Kinect.NuiSkeletonData convertSkeletonData(KinectWrapper.NuiSkeletonData data) {
 		Kinect.NuiSkeletonData dataConverted;
 		dataConverted.dwEnrollmentIndex_NotUsed = data.dwEnrollmentIndex_NotUsed;
